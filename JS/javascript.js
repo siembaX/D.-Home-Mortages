a607e580966d6b5c7d1aa55d8aec106917bf2276
Siemba Blessed <87704241+siembaX@users.noreply.github.com>
2023-01-13 16:56:44 +0200

    Add files via upload

---
+++ JS/javascript.js
@@ -0,0 +1,64 @@
+//Document Ready Function
+$(function(){
+
+    $("#avatar1").on("click",function(){
+        $("#avatar1").css({width:'20%'});
+        $("#avatar2").css({width:'15%'});
+        $("#avatar3").css({width:'15%'});
+        $("#avatar4").css({width:'15%'});
+        $("#avatar5").css({width:'12%'});
+
+        $(".whatourclients .avatar-txt1").text("“MAXINE, did the mortgage for the purchase of my home in 2017. He explained the process to me up front. He got the mortgage approved on time and met the closing date set by the sellers. Very efficient, communication was very prompt.”");
+
+
+
+    });
+    
+    $("#avatar2").on("click",function(){
+        $("#avatar1").css({width:'12%'});
+        $("#avatar2").css({width:'20%'});
+        $("#avatar3").css({width:'15%'});
+        $("#avatar4").css({width:'15%'});
+        $("#avatar5").css({width:'12%'});
+
+        $(".whatourclients .avatar-txt1").text("“SHIRI, did the mortgage for the purchase of my home in 2017. He explained the process to me up front. He got the mortgage approved on time and met the closing date set by the sellers. Very efficient, communication was very prompt.”");
+        
+    });
+
+    $("#avatar3").on("click",function(){
+        $("#avatar1").css({width:'12%'});
+        $("#avatar2").css({width:'15%'});
+        $("#avatar3").css({width:'20%'});
+        $("#avatar4").css({width:'15%'});
+        $("#avatar5").css({width:'12%'});
+
+        $(".whatourclients .avatar-txt1").text("“ADWIN A, did the mortgage for the purchase of my home in 2017. He explained the process to me up front. He got the mortgage approved on time and met the closing date set by the sellers. Very efficient, communication was very prompt.”");
+    });
+
+    $("#avatar4").on("click",function(){
+        $("#avatar1").css({width:'12%'});
+        $("#avatar2").css({width:'15%'});
+        $("#avatar3").css({width:'15%'});
+        $("#avatar4").css({width:'20%'});
+        $("#avatar5").css({width:'12%'});
+
+        $(".whatourclients .avatar-txt1").text("“RUMBIDZ, did the mortgage for the purchase of my home in 2017. He explained the process to me up front. He got the mortgage approved on time and met the closing date set by the sellers. Very efficient, communication was very prompt.”");
+    });
+
+    $("#avatar5").on("click",function(){
+        $("#avatar1").css({width:'12%'});
+        $("#avatar2").css({width:'15%'});
+        $("#avatar3").css({width:'15%'});
+        $("#avatar4").css({width:'15%'});
+        $("#avatar5").css({width:'20%'});
+
+        $(".whatourclients .avatar-txt1").text("“AIMY, did the mortgage for the purchase of my home in 2017. He explained the process to me up front. He got the mortgage approved on time and met the closing date set by the sellers. Very efficient, communication was very prompt.”");
+    });
+
+    $("#thumb1").on("click",function(){
+        
+    });
+
+
+
+});
